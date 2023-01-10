7d2d8c3d08b91c9a7294e17866150043e41f2d81
shreyag21111 <gajerashreya5@gmail.com>
2023-01-10 19:32:24 +0530

    commit 1

---
+++ app/src/main/java/com/example/telegram/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.telegram
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.telegram.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        var binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(R.layout.activity_main)
+
+        android.os.Handler().postDelayed({
+            startActivity(Intent(this,Page2_Activity::class.java))
+        },2000)
+    }
+}
